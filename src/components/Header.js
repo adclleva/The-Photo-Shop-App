92a236dd89241151b638a19351d6f715faa8ac16
ArvinLleva <arvin.lleva@yahoo.com>
2019-11-30 19:53:47 -0800

    Was able to set up the React Router for our app. To click on the Title Page on the top left in the header to go to the / route, it should display the Photos component (found in the pages folder). Added functionality to click on the shopping cart icon in the header to go to the '/cart' route, which should display the Cart component (found in the pages folder).

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <header>
-            <h2>Welcome to The Photo Shop!</h2>
-            <i className="icon ion-md-cart ri-fw ri-2x"></i>
+            <Link to="/">
+                <h2>The Photo Shop</h2>
+            </Link>
+
+            <Link to="/cart">
+                <i className="icon ion-md-cart ri-fw ri-2x"></i>
+            </Link>
+
         </header>
     )
 }
