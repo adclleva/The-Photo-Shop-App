761a6ad35e7b7443037680329d059136ec755b81
ArvinLleva <arvin.lleva@yahoo.com>
2019-12-08 01:14:02 -0800

    Able to get the toggleFavorited function to work for a specific image

--- src/context/Context.js
+++ src/context/Context.js
@@ -18,35 +18,29 @@ function ContextProvider({children}) { // we are using functional components for
             })
     }
 
-    function isFavorited(photo_id) {
-        const photosAPI = [...photos] // create a copy of the photos array 
-        const newPhotosWithFavorited = photosAPI.map(photo => {
+    function toggleFavorited(photo_id) { // we don't want to modify state directly and this is to toggle the favorite functionality
+        const newPhotosWithFavorited = photos.map(photo => {
             if (photo.id == photo_id) {
                 return {
                     ...photo,
-                    isFavorite: true
+                    isFavorite: !photo.isFavorite
                 }
             } else {
                 return photo
             }
         })
-        console.log(newPhotosWithFavorited, "isFavorited")
+
         setPhotos(newPhotosWithFavorited)
-
-    }
-
-    console.log(photos, "Context")
-
+        console.log(photos)
+    } // we want to pass this function to our image component
 
     useEffect(() => {
         getPhotos() // we call the getPhotos function to getch the data
-        .then(() => {
-            isFavorited(1)
-        })
+
     },[]) // this fetches the api and sets it into the photos by using the useEffect hook once the component renders
     
     return(
-        <Context.Provider value={{photos: photos}}> {/** here we pass in an object which is the array of photos using the value of this context provider to be accessed within the child component, in this case it ill be sent to our app*/}
+        <Context.Provider value={{photos: photos, toggleFavorited: toggleFavorited}}> {/** here we pass in an object which is the array of photos using the value of this context provider to be accessed within the child component, in this case it ill be sent to our app*/}
             {children} {/** Since this is a custom component we need to render a child prop */}
         </Context.Provider>
     )
